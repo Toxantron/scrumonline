57cb7ec784ab84bb36fc0c8805d92d62ab2b0960
Thomas Fuchs <thomas.fuchs91@gmail.com>
2015-11-30 21:13:59 +0000

    Extracted db-config from bootstrap

---
+++ src/sample-config.php
@@ -0,0 +1,9 @@
+<?php
+// database configuration parameters
+$conn = array(
+    'dbname' => 'scrum_online',
+    'user' => 'toxantron',
+    'password' => 'scrumonline',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+);

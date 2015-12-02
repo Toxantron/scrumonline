72fe6f758c4732e688b88dbcb6984762def9dd43
Thomas Fuchs <thomas.fuchs91@gmail.com>
2015-12-02 00:04:34 +0000

    Headers for session list

--- src/sessions/list.php
+++ src/sessions/list.php
@@ -9,18 +9,16 @@ include "../header.php";
   
 <div class="row">
   <div class="col-xs-12 col-md-8 col-md-offset-2">
-    <div>
-      <span class="left"><strong>Name</strong></span>
-      <span class="center"><strong>Members</strong></span>
-      <span class="right"><strong>Private</strong></span>
-    </div>
+    <span class="session-list left"><strong>Name</strong></span>
+    <span class="session-list center"><strong>Members</strong></span>
+    <span class="session-list right"><strong>Private</strong></span>
     <div class="list-group">
     <?php foreach($sessions as $session): ?>
       <a class="list-group-item" href=<?php echo ("\"session.php?id=" . $session->getId() . "\"") ?>>
-        <span class="left"><strong><?php echo $session->getName()?></strong></span>
-        <span class="center"><strong><?php echo $session->getMembers()->count() ?></strong></span>
+        <span class="session-list left"><strong><?php echo $session->getName()?></strong></span>
+        <span class="session-list center"><strong><?php echo $session->getMembers()->count() ?></strong></span>
         <?php if($session->getIsPrivate() == true): ?>
-          <span class="right"><span class="glyphicon glyphicon-lock"></span></span>
+          <span class="session-list right"><span class="glyphicon glyphicon-lock"></span></span>
         <?php endif; ?>
       </a>
     <?php endforeach ?>

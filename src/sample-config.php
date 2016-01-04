d0e42c83fae97ce5f7b55aefe03a020971e49f24
Thomas Fuchs <thomas.fuchs91@gmail.com>
2016-01-04 09:36:49 +0100

    Restored sample config

---
+++ src/sample-config.php
@@ -0,0 +1,13 @@
+<?php
+// database configuration parameters
+$conn = array(
+    'dbname' => 'scrum_online',
+    'user' => 'user',
+    'password' => 'password',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+);
+  
+$host = "www.scrumpoker.online";
+  
+$cards = [1,2,3,5,8,13,20,40,100];

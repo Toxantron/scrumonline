34f6539b41f1507c2de45a349f1e07b02e7b9e82
Toxantron <thomas.fuchs91@gmail.com>
2016-07-07 21:57:10 +0200

    Script to generate statistic plugins

--- src/controllers/statistics/README.md
+++ src/controllers/statistics/README.md
@@ -1,2 +1,7 @@
 # Scrum Poker Statistics
-This directory contains statistics of the session.
+This directory contains statistics of the session. Each statistic is implemented by a dedicated implementation of the `IStatistic` interface.
+Create new plugins by navigating to the directory and running:
+
+```sh
+$ bash generate-plugin.sh PluginName
+```
---
+++ src/controllers/statistics/generate-plugin.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+name=$1
+file=$1".php"
+echo "Generating plugin $name to $file"
+
+# Frame and docs
+echo "<?php" >> $file
+echo "/*" >> $file
+echo " * TODO: Documentation for $name" >> $file
+echo " */" >> $file
+
+# Class and method
+echo "class $name implements IStatistic" >> $file
+echo "{" >> $file 
+echo "  public function evaluate(\$session)" >> $file
+echo "  {" >> $file
+echo "    \$result = new Statistic();" >> $file
+echo "    \$result->name = \"$name\";" >> $file
+echo "    \$result->type = \"numeric\"; // Set type" >> $file
+echo "    return \$result;" >> $file
+echo "  };" >> $file
+echo "};" >> $file
+
+# Return instance
+echo "" >> $file
+echo "return new $name();" >> $file

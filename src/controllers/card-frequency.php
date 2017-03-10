0c740571d7cc6272547ebe5da02ca0cd33d39573
Toxantron <thomas.fuchs91@gmail.com>
2017-03-10 19:45:11 +0100

    Move CardFrequency to dedicated file

---
+++ src/controllers/card-frequency.php
@@ -0,0 +1,16 @@
+<?php
+// Class for frequency of a certain card value
+class CardFrequency
+{
+  function CardFrequency($value)
+  {
+    $this->value = $value;
+    $this->count = 0;
+  }
+  
+  // Value of the card
+  public $value;
+  
+  // Number of votes in poll
+  public $count;
+}

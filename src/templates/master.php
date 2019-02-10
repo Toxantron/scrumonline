12dbde13dc4eeeeea1478a3f3cc03f6e32876d67
Toxantron <thomas.fuchs91@gmail.com>
2019-02-10 14:37:42 +0100

    Offer wipe option for session

--- src/templates/master.php
+++ src/templates/master.php
@@ -4,7 +4,10 @@ include __DIR__ . "/../config.php";
 ?>
 <!-- Headline -->
 <div class="row">
-  <div class="col-sm-10 col-md-11">
+  <div class="col-xs-12 col-sm-1">
+    <button class="btn btn-lg btn-danger wipe" ng-click="master.wipe()">Wipe</button>
+  </div>
+  <div class="col-xs-10 col-sm-8 col-md-10">
     <h1>{{ master.id }} - {{ master.name }}</h1>
   </div>
   <div class="hidden-xs col-sm-2 col-md-1">
@@ -67,7 +70,7 @@ $joinUrl = $joinUrl . "{{ master.joinFragment }}";
   <!-- Team list and complete button -->
   <div class="col-xs-12 col-md-5" ng-if="!master.teamComplete">
     <h2>Team</h2>
-    <il class="list-group">
+    <ul class="list-group">
       <!-- Iterate over votes as they represent members as well -->
       <li class="list-group-item" ng-repeat="member in master.votes track by member.id">{{$index + 1}}. {{member.name}}</li>
     </ul>

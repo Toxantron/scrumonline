619bfc7d8e6a34763ba14807b876716d58087619
MarcoGreenLarsen <48754365+MarcoGreenLarsen@users.noreply.github.com>
2019-03-20 11:09:51 +0100

    Update sample-config.php

--- src/sample-config.php
+++ src/sample-config.php
@@ -15,7 +15,7 @@ $cardSets = [
     // Standard fibonaci like series of values
     ['1', '2', '3', '5', '8', '13', '20', '40', '100'],
     // Special card set with '?' for unclear stories
-    ['1', '2', '3', '5', '8', '13', '20', '40', '?'],
+    ['1', '2', '3', '5', '8', '13', '20', '40', '100', '?'],
     // Powers of two used by other teams
     ['0' ,'1', '2', '4', '8', '16', '32', '64'],
     // Card set used to estimate hours as different fractions and multiples of a working day

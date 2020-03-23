ef30ba0c533a12b712f35c9cc62341c0913da2a1
Mark Randall <randall.mark@gmail.com>
2020-03-23 18:17:49 +0000

    Removed some comments

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 cd /var/www/scrumonline
-echo Running composer update
 /usr/bin/php ./bin/composer update --no-plugins --no-scripts
-echo Running composer install
 /usr/bin/php ./bin/composer install --no-plugins --no-scripts
-echo Copying config
 cp src/sample-config.php src/config.php
 # Overwrite host
 echo '$host = "http://localhost:8080";' >> src/config.php
